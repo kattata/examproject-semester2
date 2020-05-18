96ca94dd107e6e5c73854106cc105d88fedde834
Kasia Laniecka <kasia.laniecka@gmail.com>
2020-05-18 14:39:56 +0200

    changing pictures on click done

--- js/product.js
+++ js/product.js
@@ -1,3 +1,5 @@
+// PINDOWN
+
 const photos = document.querySelector(".product-photos");
 const logo = document.querySelector(".logo");
 
@@ -6,8 +8,26 @@ const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene({
     triggerElement: photos,
     triggerHook: 0.05,
-    duration: 800,
+    duration: 870,
 })
     .setPin(photos)
-    .addTo(controller)
-    .addIndicators();
+    .addTo(controller);
+
+// CHANGE PICTURES ON CLICK
+
+const smallPictures = document.querySelectorAll(".product-img-small");
+const bigPicture = document.querySelector(".product-img-big");
+
+function changePicture(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "first-img") {
+        bigPicture.src = "../resources/images/2.3.jpg";
+    } else if (item.classList[0] === "second-img") {
+        bigPicture.src = "../resources/images/2.3.2.jpg";
+    } else {
+        bigPicture.src = "../resources/images/2.3.3.jpg";
+    }
+}
+
+smallPictures.forEach(pic => pic.addEventListener('click', changePicture));
